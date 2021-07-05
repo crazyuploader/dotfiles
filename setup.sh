c3a9a53e929b3c6e4848f5ef3f82f8f5d67fd757
crazyuploader <me@devjugal.com>
2021-07-05 16:51:33 +0530

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Script to setup ZSH with oh-my-zsh
+
+if [[ -z $(command -v zsh) ]]; then
+    echo "Installing ZSH..."
+    echo ""
+    echo "Running apt-get update..."
+    echo ""
+    sudo apt-get update
+    sudo apt-get install -y zsh curl
+fi
+
+# Installing oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Getting zsh-syntax-highlighting & zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+
+# Adding Plugins in .zshrc file
+sed -i "s/plugins=(git)/plugins=(git command-not-found ansible branch docker docker-compose gpg-agent python rsync screen themes zsh_reload zsh-autosuggestions zsh-syntax-highlighting)/" ~/.zshrc
+
+# Adding custom functions and alias
+curl -s https://raw.githubusercontent.com/crazyuploader/Misc/master/functions >> ~/.zshrc
+echo "Script Finished..."
