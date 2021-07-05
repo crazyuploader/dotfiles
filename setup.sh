c8d20b31493d846abd9bfb6dc14a40ece4a898e9
Jugal Kishore <me@devjugal.com>
2021-07-05 17:05:32 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -eu
+set -o pipefail
 
 # Script to setup ZSH with oh-my-zsh
 
@@ -8,15 +10,15 @@ if [[ -z $(command -v zsh) ]]; then
     echo "Running apt-get update..."
     echo ""
     sudo apt-get update
-    sudo apt-get install -y zsh curl
+    sudo apt-get install -y zsh curl git
 fi
 
 # Installing oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Getting zsh-syntax-highlighting & zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
 # Adding Plugins in .zshrc file
 sed -i "s/plugins=(git)/plugins=(git command-not-found ansible branch docker docker-compose gpg-agent python rsync screen themes zsh_reload zsh-autosuggestions zsh-syntax-highlighting)/" ~/.zshrc
